e07b86cb5c11c2f26c3293dea67ad18db45aa095
1mproove <99018289+1mproove@users.noreply.github.com>
2022-06-19 14:19:46 +0300

    Update PizzaStore.py

--- PizzaStore.py
+++ PizzaStore.py
@@ -2,15 +2,6 @@ from abc import ABC, abstractmethod
 
 class PizzaStore(ABC):
     def orderPizza(self, type):
-        
-    if pizza_type == 'cheese':
-        pizza = CheesePizza()
-    elif pizza_type == 'pepperoni':
-        pizza = PepperoniPizza()
-    elif pizza_type == 'clam':
-        pizza = ClamPizza()
-    elif pizza_type == 'veggie':
-        pizza = VeggiePizza()
 
         pizza.prepare()
         pizza.bake()
